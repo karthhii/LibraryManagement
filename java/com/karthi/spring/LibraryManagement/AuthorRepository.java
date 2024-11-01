2903553ec3fef1c6f97c3dd4f8b5b5cc91897ba8
karthhii <rajukarthik455@gmail.com>
2024-11-01 12:55:20 +0530

    Delete java/com/karthi/spring/LibraryManagement directory

--- java/com/karthi/spring/LibraryManagement/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.karthi.spring.LibraryManagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-
-}
--- java/com/karthi/spring/LibraryManagement/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.karthi.spring.LibraryManagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
